5cc4773ee019a1c124c27154ef3bba4e6bec2a39
Kokonodray2001 <64181896+Kokonodray2001@users.noreply.github.com>
2020-05-18 01:01:25 +0530

    brush resize features added

--- PAINT.C
+++ PAINT.C
@@ -6,15 +6,20 @@ struct color
 {
 int col;
 }block;
+struct savedfilename    // structure to save the name of all the paint files
+{  char name[20];
+}savedname[100];
+
 int show();
 void save();
 void BEEP();
-int savedindex(int);
-void nextindex();
+void saveindex();
 int getindex();
+void savename(char  *);
 union REGS in,out;
 int main()
-{   int gd=DETECT ,gm,x,y,press=0,conti;
+{  int brushsize=2;
+ int gd=DETECT ,gm,x,y,press=0,conti;
     clrscr();
      initgraph(&gd,&gm,"c:\\TURBOC3\\BGI");
      outtextxy(120,1,"press any key to exit");
@@ -24,19 +29,26 @@ int main()
      rectangle(590,10,639,475);
      rectangle(590,30,639,60);   //pen
      rectangle(590,60,639,90);   //erase
-     rectangle(590,90,639,110);  //shape
-     rectangle(590,110,615,130); //square
-     rectangle(615,110,639,130); // circle
+     rectangle(590,150,639,170);  //shape
+     rectangle(590,170,615,190); //square
+     rectangle(615,170,639,190); // circle
      rectangle(590,400,639,430); //save
      rectangle(590,430,639,460); //view
-					  //inside the box
-     outtextxy(593,95,"shapes");
+     rectangle(590,90,639,110);  //brush - small
+     rectangle(590,110,639,130);  // brush - medium
+     rectangle(590,130,639,150); // brush - large					  //inside the box
+     outtextxy(593,155,"shapes");
      outtextxy(593,415,"save");
      outtextxy(593,445,"view");
      outtextxy(592,40," pen");
      outtextxy(591,70,"erease");
-     rectangle(598,115,607,125);
-     circle(627,120,6);
+     rectangle(595,100,634,101);
+     rectangle(595,120,634,121);
+     rectangle(595,139,634,140);
+     rectangle(595,119,634,121);
+     rectangle(595,138,634,141);
+     rectangle(598,175,607,185);
+     circle(627,180,6);
     in.x.ax=1;
  int86(0x33,&in,&out);
     settextstyle(1,0,2);
@@ -47,7 +59,8 @@ int main()
 	  x= out.x.cx;
 	  y= out.x.dx;
 	if(x>=590&&y>30&&y<60&&out.x.bx==1)         //output  from mouse
-	 { press=1;                         //pen
+	 { press=1;
+				  //pen
 	 BEEP();
 	  }
 	 else if(x>=590&&y>60&&y<90&&out.x.bx==1)
@@ -66,19 +79,24 @@ int main()
 	 if(conti ==1);
 	 goto start;
 	}
-     else if(x>=590&&y>110&&y<130&&x<=615&&out.x.bx==1)          //square
-       {  BEEP();                                     //590,110,615,130
-       //	  rectangle();
+     else if(x>=590&&y>90&&y<110&&out.x.bx==1)
+       {  BEEP();
+	 brushsize=2;
        }
-      else if(x>=616&&y>110&&y<130&&out.x.bx==1) //circle
+      else if(x>=590&&y>110&&y<130&&out.x.bx==1)
       { BEEP();
-      //	circle();
+       brushsize=4;
+      }
+	else if(x>=590&&y>130&&y<150&&out.x.bx==1)
+      { BEEP();
+       brushsize=7;
       }
 
 				 //main clicking of case
       if(press==1)
     {    setcolor(0);
-       if(out.x.bx==1&&x<589&&x>2)
+	settextstyle(1,0,brushsize);
+       if(out.x.bx==1&&x<585&&x>50)
 	outtextxy(x,y,".");
        }
        else if(press==2)
@@ -86,17 +104,16 @@ int main()
        //	int86(0x33,&in,&out);
       // { rectangle(x-1,y-1,x+1,y+1); }
       setcolor(15);
-	settextstyle(1,0,4);
-      if(out.x.bx==1&&x<589&&x>2)
-       {
+	settextstyle(1,0,brushsize);
+      if(out.x.bx==1&&x<581&&x>50)
 	 outtextxy(x,y,"*");
-	}
        }
        }
 	getch();
 
 
        closegraph();
+
  return 0;
 }
 void BEEP()
@@ -104,12 +121,13 @@ void BEEP()
  delay(100);
  nosound();
  }
- void save()
+ void save()   //saving the file
  {  int i,j;
     char filename[20];
       FILE *fp;
+       saveindex();
     gets(filename);
-    // a= getindex();
+     savename( filename );   //save the name of the txt in a file
       fp=fopen(filename,"w");
     for(i=10;i<475;i++)
    { for(j=50;j<590;j++)
@@ -122,15 +140,35 @@ void BEEP()
     fclose(fp);
   //  nextindex();
    BEEP();
-}
-
+   }
 int show()
 {
-  int i,j;
+  int r,q, i=60,j=50,check,l=0;
+  char p[20],s[100];
   char savedfile[20];
-    FILE *fp;
+  FILE *save;
+  FILE *fp;
+ // r= getindex();
+BEEP();
+     setcolor(15);
+   setfillstyle(SOLID_FILL,15);
+  floodfill(450,450,15);
+    save = fopen("savedname.txt","r");
+       setcolor(4);
+    settextstyle(1,0,2);
+    fseek(0,0,0);
+  while(!feof(save))	//feof() returns will return 0 if file pointer is at end of file else it will rteturn zero
+  { for(q=0;q<4;q++)                     //while(0) terminates the loop as zero is false.. so not operator(!) turns 0 ->1
+  {       //drawing box around the the name of the saved files
+      fscanf(save,"%s",&s) ;
+      if(!feof(save))
+   { outtextxy(i,j,s);
+  rectangle(i-4,j,i+95,j+22);
+  i= i +100;}
+   } j=j+25;i=60;
+     }
+     fclose(save);
   gets(savedfile);
-   ;
    fp = fopen(savedfile,"r");
        for(i=10;i<475;i++)
    { for(j=50;j<590;j++)
@@ -143,36 +181,25 @@ int show()
    getch();
   return 1;
 }
- /*int getindex()
-{ int a;
-     FILE *index;
-     index = fopen("index.txt","r");
-     fscanf(index,"%d",&a);
-     fclose(index);
- return a;
-} * /
-/* void nextindex()
- {
-    int a;
-    FILE *index;
-    a = getindex();
-     index = fopen("index.txt","w");
-     fprintf(index,"%d",(a+1));
-     fclose(index);
-} */
-/*int savedindex(int n)
-{  int i,a;
-// char ch[5];
-// setcolor(0);
-  // setfillstyle(SOLID_FILL,0);
-// rectangle(290,225,350,275);
-// floodfill(295,350,7);
- // settextstyle(1,0,0);
- // outtextxy(292,224,"select the file number to view");
- // for(i=0;i<=n;i++)
- // {   itoa(n,ch,5);
-  // outtextxy(292+i,224+i,ch);
-//  }
-    scanf("%d",&a);
-  return a;
-} */
+void savename(char *p)     //called each time with func save() to save the name file name in file
+{  FILE *save;
+  save = fopen("savedname.txt","a");    //appends the name of the new files
+  fprintf(save,"%s ",p);       //coders- in file the words are stored as char so they pointers stops ehen an space is encountered
+  fclose(save);
+}
+void saveindex()
+{  int n;
+  FILE *next;
+    next = fopen("gettingindex.txt","w");
+   n =  getindex();
+   fprintf(next,"%d",n+1);
+    fclose(next);
+}
+int getindex()
+{   int n;
+   FILE *current;
+   current = fopen("gettingindex.txt","r");
+     fscanf(current,"%d",n);
+     fclose(current);
+     return n;
+}
